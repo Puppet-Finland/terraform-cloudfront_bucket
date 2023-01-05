efaf5ed0bd5e6f67d4b240fa06b1880561d657ab
Samuli Seppänen <samuli.seppanen@puppeteers.net>
2023-01-05 19:31:01 +0200

    Add bucket policy
    Signed-off-by: Samuli Seppänen <samuli.seppanen@puppeteers.net>

--- bucket.tf
+++ bucket.tf
@@ -14,3 +14,8 @@ resource "aws_s3_bucket_versioning" "default" {
     status = "Disabled"
   }
 }
+
+resource "aws_s3_bucket_policy" "default" {
+  bucket = aws_s3_bucket.default.id
+  policy = templatefile("${path.module}/bucket_policy.json.tftpl", { bucket_arn = aws_s3_bucket.default.arn, source_arn = aws_cloudfront_distribution.default.arn })
+}
